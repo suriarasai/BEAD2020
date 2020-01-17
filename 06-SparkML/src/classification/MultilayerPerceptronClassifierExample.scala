80f403422e5a105f7545adc1cad9228d907dca62
Suria R Asai <suria.r.asai@gmail.com>
2020-01-17 13:16:47 +0800

    Update MultilayerPerceptronClassifierExample.scala
    file path

--- 06-SparkML/src/classification/MultilayerPerceptronClassifierExample.scala
+++ 06-SparkML/src/classification/MultilayerPerceptronClassifierExample.scala
@@ -16,7 +16,7 @@ object MultilayerPerceptronClassifierExample {
 
     // Load the data stored in LIBSVM format as a DataFrame.
     val data = spark.read.format("libsvm")
-      .load("/home/cloudera/git/BEAD-SEP19/W07-SparkML/data/classification/sample_multiclass_classification_data.txt")
+      .load("/home/cloudera/git/BEAD2020/06-SparkML/data/classification/sample_multiclass_classification_data.txt")
 
     // Split the data into train and test
     val splits = data.randomSplit(Array(0.6, 0.4), seed = 1234L)
